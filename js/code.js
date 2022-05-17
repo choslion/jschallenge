516d017008471d624cf07a026b47d123e9d87a82
choslion <102371077+choslion@users.noreply.github.com>
2022-05-17 16:21:49 +0900

    finish number game

--- js/code.js
+++ js/code.js
@@ -1,26 +1,25 @@
 const form = document.querySelector("#randomForm");
 const choInput = document.querySelector("#choInput");
 const guessInput = document.querySelector("#guessInput");
-const playBtn = document.querySelector("#playBtn");
 const result = document.querySelector(".result");
 
-function excute(guessNum , maxInput){
 
-if(guessNum === parseInt(maxInput)){   
+function execute(guessNum, ranNum){
+if(parseInt(guessNum) === ranNum){   
     result.classList.remove("result");
-    const ranNum = Math.floor(Math.random()) * maxInput + 1;
-    result.innerHTML = `You chose ${guessNum} , the machine chose ${ranNum} \nYou Won!`;
-} else if (guessNum !== parseInt(maxInput)){ 
+    result.innerHTML = `You chose ${guessNum} , the machine chose ${ranNum} \n You Won!`;
+} else { 
     result.classList.remove("result");
-    result.innerHTML = `You chose ${guessNum} , the machine chose ${ranNum}\nYou lost!`;
+    result.innerHTML = `You chose : ${guessNum} , the machine chose : ${ranNum}\n\ You lost!`;
 }
 }
 
 function formFunc(event) {
     event.preventDefault();
-    const guessNum = guessInput.value;
     const maxInput = choInput.value;
-    execute(guessNum , maxInput);
+    const guessNum = guessInput.value;
+    const ranNum = Math.ceil(Math.random()* maxInput-0.5 );
+    execute(guessNum , ranNum);
 }
 
 form.addEventListener("submit" , formFunc);
